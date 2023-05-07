22b9b8032ea7ef2548693c899788d52d8c80730a
xyz <2454352843@qq.com>
2023-05-07 10:43:30 +0800

    5.7 服务器

--- Pytorch/O3_estimate/Resource/config.py
+++ Pytorch/O3_estimate/Resource/config.py
@@ -1,22 +1,4 @@
-#数据集超参n
-n = 3
-img_size = (n,n)
+img_size = (200,200)
 ratio = 0.01
-base_path = r'E:\data\5-mouth-dataset'
 
-#文件栅格信息
-tif_x,tif_y = 6159, 3541
-
-#读取文件顺序
-tif_list = ['GEOS', 'LandCover', 'SILAM', 'Tropomi']
-# GEOS_list = ['PS','T2M','TROPCOL_O3','U2M','V2M','ZPBL']
-GEOS_list = ['PS','T2M','TROPCOL_O3','U2M','V2M','ZPBL']
-LandCover = ['DEM','NDVI','POP','pri_sec']
-
-# ST
-math_path_st = r'Resource/math_ST.txt'
-# 数据集位置顺序
-
-arr_list = ['PS', 'T2M', 'TROPCOL_O3', 'U2M', 'V2M', 'ZPBL', 'cnc_O3', 'TropOMI_O3_PR', 'DEM', 'NDVI', 'POP', 'pri_sec']
-
-batch_size = 128
+base_path = r'G:\2021-ck-xyz\data\5-mouth-dataset'
